c222a90d4a3a20ecf1079531f47e4fec879b871c
518H0090 <73675587+518H0090@users.noreply.github.com>
2021-10-28 15:48:50 +0700

    Reauthen and Update Password

--- Code/FinalProject_518H0090/app/src/main/java/tdtu/com/finalproject/fragment/ChangePasswordFragment.java
+++ Code/FinalProject_518H0090/app/src/main/java/tdtu/com/finalproject/fragment/ChangePasswordFragment.java
@@ -1,30 +1,35 @@
 package tdtu.com.finalproject.fragment;
 
+import android.app.Dialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import tdtu.com.finalproject.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ChangePasswordFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ChangePasswordFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    View view;
+    EditText passwordChange, rePasswordChange;
+    Button btnUpdatePassword;
+    FirebaseUser user;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public ChangePasswordFragment() {
         // Required empty public constructor
@@ -34,38 +39,109 @@ public class ChangePasswordFragment extends Fragment {
         return new ChangePasswordFragment();
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChangePasswordFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ChangePasswordFragment newInstance(String param1, String param2) {
-        ChangePasswordFragment fragment = new ChangePasswordFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_change_password, container, false);
+        view = inflater.inflate(R.layout.fragment_change_password, container, false);
+        declareView();
+        ClickUpdatePassWordFireBase();
+
         return view;
     }
+
+    private void declareView() {
+        passwordChange = view.findViewById(R.id.passwordChange);
+        rePasswordChange = view.findViewById(R.id.rePasswordChange);
+        btnUpdatePassword = view.findViewById(R.id.btnUpdatePassword);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+    }
+
+    private void ClickUpdatePassWordFireBase() {
+        btnUpdatePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UpdatePasswordFirebase();
+            }
+        });
+    }
+
+    private void UpdatePasswordFirebase() {
+        String password = passwordChange.getText().toString().trim();
+        String repassword = rePasswordChange.getText().toString().trim();
+
+        if (password.isEmpty() || repassword.isEmpty()) {
+            Toast.makeText(getActivity(), "Vui Lòng Không Để Trống", Toast.LENGTH_SHORT).show();
+        } else if (!password.equals(repassword)) {
+            Toast.makeText(getActivity(), "Mật Khẩu Không Trùng Khớp", Toast.LENGTH_SHORT).show();
+        } else {
+            String newPassword = password;
+
+            user.updatePassword(newPassword)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getActivity(), "Cập Nhật Mật Khẩu Thành Công", Toast.LENGTH_SHORT).show();
+                            } else {
+                                reAuthenticateUserDialog();
+                            }
+                        }
+                    });
+        }
+
+
+    }
+
+    private void reAuthenticateUserDialog() {
+        Dialog dialog = new Dialog(getActivity());
+        dialog.setContentView(R.layout.dialog_show_reauthentication);
+
+        EditText reUsername = dialog.findViewById(R.id.reUsername);
+        EditText rePassword = dialog.findViewById(R.id.rePassword);
+        Button btnReLogin = dialog.findViewById(R.id.btnReLogin);
+        Button btnReLoginCancel = dialog.findViewById(R.id.btnReLoginCancel);
+
+        btnReLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = reUsername.getText().toString().trim();
+                String password = rePassword.getText().toString().trim();
+
+                if (username.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(getActivity(), "Thông tin không được để trống", Toast.LENGTH_SHORT).show();
+                } else {
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                    AuthCredential credential = EmailAuthProvider
+                            .getCredential(username, password);
+
+                    user.reauthenticate(credential)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(getActivity(), "Xác thực thành công", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Toast.makeText(getActivity(), "Xác thực thất bại", Toast.LENGTH_SHORT).show();
+                                    }
+
+                                }
+                            });
+
+                }
+            }
+        });
+
+        btnReLoginCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Hủy Xác Thực", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+
 }
