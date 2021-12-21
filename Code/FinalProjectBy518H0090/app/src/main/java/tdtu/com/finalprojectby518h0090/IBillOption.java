ee626b1a3229434d441da15b0a6156771b6089cb
518H0090 <73675587+518H0090@users.noreply.github.com>
2021-12-21 07:41:07 +0700

    Khóa source

--- Code/FinalProjectBy518H0090/app/src/main/java/tdtu/com/finalprojectby518h0090/IBillOption.java
+++
@@ -1,8 +0,0 @@
-package tdtu.com.finalprojectby518h0090;
-
-import tdtu.com.finalprojectby518h0090.model.Bill;
-
-public interface IBillOption {
-    void onClickEdit(Bill bill);
-    void onClickDelete(Bill bill);
-}
